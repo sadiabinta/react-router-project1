179542d9f1f4300d54a658fba56b19af31d64fdc
Sadia Binta Kabir <sadiaa.kabir@gmail.com>
2023-04-08 13:57:31 -0400

    untill param

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Root from './routes/root'
+import Root, {loader as rootLoader, action as rootAction} from './routes/root'
+import ErrorPage from './ErrorPage'
+import Contacts from './routes/Contacts'
+import { loader } from './routes/root'
 
 const router=createBrowserRouter([
   {
     path:'/',
-    element:<Root></Root>
-  },
+    element:<Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    loader: rootLoader,
+    action: rootAction,
+    children:[
+      {
+        path:'/contacts/:contactId',
+        element:<Contacts></Contacts>
+      }
+    ]
+  }
+  
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
