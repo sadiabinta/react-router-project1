6a2e98e9971c7974e7aaa430080fdd64f0d54d8a
Sadia Binta Kabir <sadiaa.kabir@gmail.com>
2023-04-08 11:26:18 -0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './routes/root'
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Root></Root>
+  },
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
